399a26e8e9c8efffa840d502da7711666b57aae3
kruemelmann <kruemelmannlp@gmail.com>
2021-12-26 01:13:21 +0100

    working 2021-12-25-nasa-api-setup to add copyright and title to the picture of the day

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,3 +1,6 @@
+//TODO
+// add a cache to the get so if i call an api like nasa for example the pic of the day. I don't waist API-calls on loading the same image over and over again.
+
 function httpGet(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
